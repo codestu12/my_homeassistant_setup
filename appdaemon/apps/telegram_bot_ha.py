201ccbfb21a2b5d8156e7b9f5f72071ac87e86f7
Stuart <stuartgorman@hotmail.com>
2020-04-26 15:26:32 +1000

    Changed aircon and alarm set to argument commands

--- appdaemon/apps/telegram_bot_ha.py
+++ appdaemon/apps/telegram_bot_ha.py
@@ -1,5 +1,11 @@
 import appdaemon.plugins.hass.hassapi as hass
 import pprint
+import six
+
+ALARM_STATES = ['disarmed', 'armed_night', 'armed_away', 'armed_home']
+ALARM_SERVICES = ['alarm_disarm', 'alarm_arm_night', 'alarm_arm_away', 'alarm_arm_home']
+
+
 
 class TelegramBotEventListener(hass.Hass):
     """Event listener for Telegram bot events."""
@@ -27,19 +33,23 @@ class TelegramBotEventListener(hass.Hass):
         self.listen_event(self.receive_telegram_callback, 'telegram_callback')
         self.listen_event(self.receive_telegram_text, "telegram_text")
         
-    def process_commands(self, command, user_id, user_name):
+    def process_commands(self, command, user_id, user_name, args):
         ret = True
         msg = None
         keyboard = None
         
         if command == "/hello" or command == "/help":
-            msg, keyboard = self.hello_command(user_id, user_name)
+            msg, keyboard = self.hello_command(user_id, user_name, args)
         elif command == "/aircon" and self.aircon_configured:
-            msg, keyboard = self.aircon_command(user_id, user_name)
+            msg, keyboard = self.aircon_command(user_id, user_name, args)
+        elif command == "/aircon_set" and self.aircon_configured: 
+            msg, keyboard = self.aircon_set_command(user_id, user_name, args)
         elif command == "/alarm" and self.alarm_configured:
-            msg, keyboard = self.alarm_command(user_id, user_name)
+            msg, keyboard = self.alarm_command(user_id, user_name, args)
+        elif command == "/alarm_set" and self.alarm_configured:
+            msg, keyboard = self.alarm_set_command(user_id, user_name, args)    
         elif command == "/temps" and self.temperature_configured:
-            msg, keyboard = self.temps_command(user_id, user_name)
+            msg, keyboard = self.temps_command(user_id, user_name, args)
         elif command == "/do_nothing":
             msg = "Goodbye"
         else:
@@ -59,7 +69,7 @@ class TelegramBotEventListener(hass.Hass):
         
         return ret, msg, keyboard 
 
-    def hello_command(self, user_id, user_name):
+    def hello_command(self, user_id, user_name, args):
         msg = "Hello {}. What would you like to know?:".format(user_name)
         
         #decide what buttons to display
@@ -85,8 +95,9 @@ class TelegramBotEventListener(hass.Hass):
             keyboard.append(row)
                                                     
         return msg, keyboard
-            
-    def aircon_command(self, user_id, user_name):
+    
+    #Info about aircon and keyboard for aircon actions
+    def aircon_command(self, user_id, user_name, args):
         aircon_state = self.get_state(self.args["aircon"])
         aircon_current_temp = self.get_state(self.args["aircon"], 'current_temperature')
         aircon_setpoint_temp = self.get_state(self.args["aircon"], 'temperature')
@@ -95,12 +106,23 @@ class TelegramBotEventListener(hass.Hass):
         if aircon_state in ["cool", "heat", "heat_cool"]:
             msg += "Setpoint is {} degrees".format(aircon_setpoint_temp)
               
-        keyboard = [[("Off", "/aircon_set off"), ("Fan", "/aircon_set fan"), ("Cool", "/aircon_set cool")],
+        keyboard = [[("Off", "/aircon_set off"), ("Fan", "/aircon_set fan_only"), ("Cool", "/aircon_set cool")],
                     [("Heat", "/aircon_set heat"), ("Goodbye", "/do_nothing")]]
-                                                    
-        return msg, keyboard
             
-    def alarm_command(self, user_id, user_name):
+        return msg, keyboard
+    
+    #Changing aircon state
+    def aircon_set_command(self, user_id, user_name, args):
+        msg = "Unknown aircon mode"
+        aircon_supported_modes = self.get_state(self.args["aircon"], 'hvac_modes')
+        if len(args) == 1:
+            if args[0] in aircon_supported_modes:
+                self.call_service('climate/set_hvac_mode', entity_id = self.args["aircon"], hvac_mode=args[0])
+                msg= 'Aircon changed to {}'.format(args[0])
+        return msg, None
+        
+    #Info about alarm and keyboard for alarm actions        
+    def alarm_command(self, user_id, user_name, args):
         alarm_state = self.get_state(self.args["alarm"]["control"])
         
         msg = "Alarm is currently {}\n".format(str(alarm_state).replace("_", " "))
@@ -132,14 +154,39 @@ class TelegramBotEventListener(hass.Hass):
                             this_sensor_state = self.args["alarm"]["state_mapping"][this_sensor_device_class][this_sensor_state]
                 msg += "{}: {}\n".format(this_sensor_name, this_sensor_state)
         return msg
-        
-    def alarm_set(self, user_id, user_name):
+    
+    #setup for changing alarm state. Prompts user for pin
+    def alarm_set(self, user_id, user_name, args):
         self.pending_alarm_state = None
-        
+        msg = "Unknown alarm state"
+        if len(args) == 1:
+            if args[0] in ALARM_STATES:
+                self.pending_alarm_state = args[0]
+                self.receiving_alarm_pin = True
+                msg = "Enter alarm pin"
+        return msg, None
+    
+    #calls service to change alarm state
     def action_alarm_pin(pin):
-        pass
+        msg = "Pin is incorrect"
+        if isinstance(pin, six.string_types) and  len(pin) < 12 and pin.isalnum():
+            #get values for service call
+            alarm_instance = self.args["alarm"]["control"]
+            alarm_control_parent = alarm_instance.split('.')[0]
             
-    def temps_command(self, user_id, user_name):
+            #call alarm set service
+            service_name = "{}/{}".format(alarm_control_parent, ALARM_SERVICES[ALARM_STATES.index(self.pending_alarm_state)])
+            self.call_service(service_name, entity_id = alarm_instance, code=pin)
+            
+            #check that something happened
+            alarm_state = self.get_state(self.args["alarm"]["control"])
+            if alarm_state == self.pending_alarm_state:
+                msg = "Alarm state changed"
+        self.pending_alarm_state = None
+        return msg 
+        
+    #prints current temperatures
+    def temps_command(self, user_id, user_name, args):
         temp_group = self.get_state(self.args["temp_group"], "entity_id")
         
         msg = "Temperatures: \n"
@@ -151,13 +198,20 @@ class TelegramBotEventListener(hass.Hass):
         
         return msg, None
 
+    #telegram command received
     def receive_telegram_command(self, event_id, payload_event, *args):
         assert event_id == 'telegram_command'
         user_id = payload_event['user_id']
         command = payload_event['command']
         user_name = payload_event['from_first']
+        cmd_args = []
+        
+        if(' ' in command):
+            cmd_bits = command.split(' ')
+            command = cmd_bits[0]
+            cmd_args = cmd_bits[1:]
 
-        ret, msg, keyboard = self.process_commands(command, user_id, user_name)
+        ret, msg, keyboard = self.process_commands(command, user_id, user_name, cmd_args) 
         if ret:
             self.call_service(
                 'telegram_bot/send_message',
@@ -166,16 +220,23 @@ class TelegramBotEventListener(hass.Hass):
                 disable_notification=True,
                 inline_keyboard=keyboard)
 
+    #response to previous message
     def receive_telegram_callback(self, event_id, payload_event, *args):
         assert event_id == 'telegram_callback'
         user_id = payload_event['user_id']
         user_name = payload_event['from_first']
         data_callback = payload_event['data']
         callback_id = payload_event['id']
+        cmd_args = []
+        
+        if(' ' in data_callback):
+            cmd_bits = data_callback.split(' ')
+            data_callback = cmd_bits[0]
+            cmd_args = cmd_bits[1:]
         
         #send_messaage is for chat window replies, answer_callback_query is for banner notification type responses
         
-        ret, msg, keyboard = self.process_commands(data_callback, user_id, user_name)
+        ret, msg, keyboard = self.process_commands(data_callback, user_id, user_name, cmd_args)
         if ret:
             self.call_service(
                 'telegram_bot/send_message',
@@ -191,6 +252,7 @@ class TelegramBotEventListener(hass.Hass):
                     message=msg,
                     callback_query_id=callback_id)
 
+    #Non-command, non-callback response, regular text
     def receive_telegram_text(self, event_id, payload_event, *args):
         user_id = payload_event['user_id']
         user_name = payload_event['from_first']
@@ -198,8 +260,9 @@ class TelegramBotEventListener(hass.Hass):
         msg_id = payload_event["id"]
         chat_id = payload_event["chat_id"]
         
+        #check for alarm pin sent at prompt
         if self.receiving_alarm_pin:
-            self.action_alarm_pin(query_text)
+            msg = self.action_alarm_pin(query_text)
             
             #delete message for security
             self.call_service(
@@ -209,7 +272,18 @@ class TelegramBotEventListener(hass.Hass):
                 message="******",
                 inline_keyboard=None
             )
+            
+            #send response
+            self.call_service(
+                'telegram_bot/send_message',
+                target=user_id,
+                message=msg,
+                disable_notification=True,
+                inline_keyboard=None)
+                    
             self.receiving_alarm_pin = False
+            
+        #regular message/enquiry
         else:
             #received question or random message
             msg = "Sorry, I didn't understand that."
